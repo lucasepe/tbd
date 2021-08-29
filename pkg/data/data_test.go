6b34a435d25e91a791ae5af28a92aefcf5bd73f3
Luca Sepe <luca.sepe@gmail.com>
2021-08-29 09:51:01 +0200

    fix broke test

--- pkg/data/data_test.go
+++ pkg/data/data_test.go
@@ -26,12 +26,12 @@ func TestFetchFromURI(t *testing.T) {
 }
 
 func TestFetchFromFile(t *testing.T) {
-	data, err := FetchFromFile("../../testdata/sample1.yml.tbd", 10)
+	data, err := FetchFromFile("../../testdata/sample.tbd", 10)
 	if err != nil {
 		t.Error(err)
 	}
 
-	want := `---apiVer`
+	want := `{{ greetin`
 	if got := flatten(string(data)); got != want {
 		t.Errorf("got [%v] want [%v]", got, want)
 	}
